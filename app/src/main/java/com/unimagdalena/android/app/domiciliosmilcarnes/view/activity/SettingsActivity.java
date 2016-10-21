f0c23dd9231b3887af49074e48535a0a8aadba3e
receducativos <alberto9.24.93>
2016-10-21 12:43:26 -0500

    Added other views

---
+++ app/src/main/java/com/unimagdalena/android/app/domiciliosmilcarnes/view/activity/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.unimagdalena.android.app.domiciliosmilcarnes.view.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.unimagdalena.android.app.domiciliosmilcarnes.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
